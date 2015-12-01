e103820c8b6863a85afd64886748d3affc0fa6d4
Christopher Pearson <pearsonchristopherc@gmail.com>
2015-12-01 15:23:49 -0800

    cleaned up code

--- dfhamper.lua
+++ dfhamper.lua
@@ -1,6 +1,16 @@
 --scripts/hamper.lua
 --finds all clothes in stockpile and workshop
---origiinal author: Christoth
+--original author: Christoth
+
+--[[=begin
+
+DFHamper
+========
+Searches through your stockpiles as well as clothier and leather work
+workshops for unowned articles of clothing, and returns a count of each
+item type found. 
+
+=end]]
 
 local utils = require('utils')
 local args = {...}
@@ -22,53 +32,51 @@ tunics = 0;
 vests = 0;
 caps = 0;
 hoods = 0;
+----------------------------------
 --find all stockpiles
 --find all clothier and leatherworker shops
 local piles = {}
 local shops = {}
+--grab all stockpiles, clothiers, and leather workshops and put in tables
 for k, v in ipairs(df.global.world.buildings.all) do
   local str = utils.getBuildingName(v)
   if string.find(str, "Stockpile") then
-    --print('stockpile found: '..str)
+    
     table.insert(piles, v)
-  
- 
-  
+    
   elseif string.find(str, "Clothier") or string.find(str, 'Leather') then
-    --print('workshop found '..str)
+    
     table.insert(shops, v)
   end
 end
 
 --search through piles and shops for clothes
 for k, v in ipairs(piles) do
-  --print(v)
+  
   local contents = dfhack.buildings.getStockpileContents(v)
   
   for i, j in ipairs(contents) do
-    --print(j._type)
-    --local stra = get_type(j)
-    --print(stra)
+    
     local thing = get_type(j)
-    --print(thing)
+    --check if bin and look inside if
     if thing == 'bin' then
-      --print('found a bin')
-      --print(thing)
+      
       local bincon = dfhack.items.getContainedItems(j)
       for m, n in ipairs(bincon) do
         count_clothes(n)
-        --print('got here')
-      end
-    end
-    count_clothes(j)
         
+      end
+    --otherwise count 
+    else
+      count_clothes(j)
+    end  
   end
 end
 
 for k, v in ipairs(shops) do
- -- print(v)
+ 
   local contents = v.contained_items
-  --print(contents)
+ 
   
   for i=0, #contents-1 do
     local contained = contents[i]
@@ -76,35 +84,35 @@ for k, v in ipairs(shops) do
   end
 end
 
+--display results
 displaycount()
 
+--functions
+---------------------------
 function get_type(item)
   return string.match(tostring(item._type),"<type: item_(.+)st>"):lower()
 end
-
+---------------------------
 function count_clothes(item)
-  --print(item)
+  
   if dfhack.items.getOwner(item) == nil then
   local chkstr = get_type(item)
-  --print(chkstr)
+  
   if chkstr == 'shoes' then
-   -- print('found shoes')
-    --print(item.subtype.name)
+   
     if item.subtype.name == 'sock' then
-     -- print('found a sock')
+     
       socks = socks+1
      
     elseif item.subtype.name == 'shoe' then
-     -- print('found a shoe')
+     
       shoes = shoes+1
     end
   
   elseif chkstr == 'gloves' then
-    --print('found gloves')
-   -- print(item.subtype.name)
-    --print(item.getGloveHandedness(item))
+    
     if item.subtype.name == 'glove' then
-     -- print('found a glove')
+     
       if item.getGloveHandedness(item) == 1 then
         Lgloves = Lgloves + 1
       elseif item.getGloveHandedness(item) == 2 then
@@ -112,7 +120,7 @@ function count_clothes(item)
       end
       gloves = gloves + 1
     elseif item.subtype.name == 'mitten' then
-    --  print('found a mitten')
+    
       if item.getGloveHandedness(item) == 1 then
         Lmittens = Lmittens + 1
       elseif item.getGloveHandedness(item) == 2 then
@@ -122,16 +130,15 @@ function count_clothes(item)
     end
     
   elseif chkstr == 'armor' then
-    --print('found armor')
-    --print(item.subtype.name)
+    
     if item.subtype.name == 'dress' then
-    --   print('found a dress')
+    
       dresses = dresses + 1
     elseif item.subtype.name == 'robe' then
-    --  print('found a robe')
+    
       robes = robes + 1
     elseif item.subtype.name == 'cloak' then
-      --print('found a cloak')
+      
       cloaks = cloaks + 1
     elseif item.subtype.name == 'tunic' then
       tunics = tunics + 1
@@ -140,7 +147,7 @@ function count_clothes(item)
     end
     
   elseif chkstr == 'helm' then
-    --print('found helm')
+   
     if item.subtype.name == 'cap' then
       caps = caps + 1
     elseif item.subtype.name == 'hood' then
@@ -156,7 +163,7 @@ function count_clothes(item)
   end  
    
 end
-
+----------------------------------------------
 
 function displaycount()
   print('all unowned items of clothing found in stockpiles and workshops:')
@@ -197,3 +204,4 @@ function displaycount()
   print('found '..hoods..' hoods')
   print('found '..trousers..' trousers')
 end
+------------------------------------------------------
